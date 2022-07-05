cedc0ede1a6fb4829330c87a9134769100e11766
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-07-05 09:52:43 +0300

    used Encapsulation

--- classesWithAttribute/src/ProductManager.java
+++ classesWithAttribute/src/ProductManager.java
@@ -1,5 +1,11 @@
+import java.util.stream.Stream;
+
 public class ProductManager {
-    public void Add(Product product){
-        System.out.println("ürün eklendi: " + product.name);
+    public void Add(Product product) {
+        System.out.println("ürün eklendi: " + product.getName());
+    }
+
+    public void Add2() {
+
     }
 }
